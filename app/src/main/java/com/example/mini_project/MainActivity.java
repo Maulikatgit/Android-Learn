5918f82b9fb44f9424c47c4e8f18bad18b60ebff
hn3parmar <hardikp_dev@outlook.com>
2023-04-13 14:32:55 +0530

    Main Screen UI Changed

--- app/src/main/java/com/example/mini_project/MainActivity.java
+++ app/src/main/java/com/example/mini_project/MainActivity.java
@@ -10,7 +10,7 @@ import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public CardView card1, card2, card3, card4, card5;
+    public CardView card1, card2, card3, card4, card5, card6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +22,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         card3 = (CardView) findViewById(R.id.cv3);
         card4 = (CardView) findViewById(R.id.cv4);
         card5 = (CardView) findViewById(R.id.cv5);
+        card6 = (CardView) findViewById(R.id.cv6);
 
         card1.setOnClickListener(this);
         card2.setOnClickListener(this);
         card3.setOnClickListener(this);
         card4.setOnClickListener(this);
         card5.setOnClickListener(this);
+        card6.setOnClickListener(this);
 
     }
 
@@ -43,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 i = new Intent(this, topics.class);
                 startActivity(i);
                 break;
-            case R.id.cv5:
+            case R.id.cv6:
                 Uri uri = Uri.parse("https://github.com/Maulikatgit/Android-Learn");
                 i = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(Intent.createChooser(i, "Share Application"));
