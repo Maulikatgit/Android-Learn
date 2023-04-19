f3c454716c3af7579dcee52733a06dc20ab5e045
Khushi <khushi@awesome_lavender.com>
2023-04-19 23:13:17 +0530

    content provider

---
+++ app/src/main/java/com/example/mini_project/content_provider.java
@@ -0,0 +1,14 @@
+package com.example.mini_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class content_provider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_provider);
+    }
+}
