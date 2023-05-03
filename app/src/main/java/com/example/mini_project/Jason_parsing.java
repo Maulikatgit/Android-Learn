7594ac6ee677f85999d7114ccae841234cc0f467
hn3parmar <mm.2004.parmar@gmail.com>
2023-05-03 14:32:16 +0530

    All set

---
+++ app/src/main/java/com/example/mini_project/Jason_parsing.java
@@ -0,0 +1,14 @@
+package com.example.mini_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Jason_parsing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jason_parsing);
+    }
+}
